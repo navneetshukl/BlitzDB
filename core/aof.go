146e4c2c570f0edce3c2999fbaab16841e20385e
navneetshukl <navneetshukla824@gmail.com>
2025-11-24 21:11:08 +0530

    encoding and incr

--- core/aof.go
+++ core/aof.go
@@ -9,22 +9,23 @@ import (
 	"github.com/blitzdb/blitz/config"
 )
 
-// TODO: Support non-kv data structure
+// TODO: Support Expiration
+// TODO: Support non-kv data structures
 // TODO: Support sync write
-
-func dumpKey(fp *os.File,key string,obj *Obj){
-	cmd:=fmt.Sprintf("SET %s %s",key,obj.Value)
-	tokens:=strings.Split(cmd," ")
-	fp.Write(Encode(tokens,false))
+func dumpKey(fp *os.File, key string, obj *Obj) {
+	cmd := fmt.Sprintf("SET %s %s", key, obj.Value)
+	tokens := strings.Split(cmd, " ")
+	fp.Write(Encode(tokens, false))
 }
 
+// TODO: To to new and switch
 func DumpAllAOF() {
 	fp, err := os.OpenFile(config.AOFFile, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
 	if err != nil {
-		log.Print("error", err)
+		fmt.Print("error", err)
 		return
 	}
-	log.Println("rewriting AOF file at ", config.AOFFile)
+	log.Println("rewriting AOF file at", config.AOFFile)
 	for k, obj := range store {
 		dumpKey(fp, k, obj)
 	}
